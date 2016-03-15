751bf199b7be882ef9845000409cb1b03f0ed95d
twotymz <joshua.p.may@gmail.com>
2016-03-15 07:54:48 -0500

    Added header decoding

--- sms.py
+++ sms.py
@@ -3,31 +3,41 @@ import sys
 import os
 import getopt
 import numpy
-import mapper
-import cpu
+import pprint
 
 rom = None
+header = {}
+
+def _read_header () :
+
+    global rom
+    global header
+
+    header['raw'] = ''.join (['{0:02X} '.format (r) for r in rom[0x7FF0:0x8000]])
+    header['tmr_sega'] = ''.join ([chr (r) for r in rom[0x7FF0:0x7FF8]])
+    header['checksum'] = rom[0x7FFA] | rom[0x7FFB] << 8
+    header['product_code'] = rom[0x7FFC] | rom[0x7FFD] << 8 | ((rom[0x7FFE] & 0xF0) >> 4) << 16
+    header['version'] = rom[0x7FFE] & 0xF
+    header['region'] = (rom[0x7FFF] & 0xF0) >> 4
+    header['size'] = rom[0x7FFF] & 0xF
+
 
 def readRom (path) :
 
     global rom
     rom = None
 
-    try :
+    file_size = os.path.getsize (path)
 
-        file_size = os.path.getsize (path)
+    with open (path, 'rb') as f :
 
-        with open (path, 'rb') as f :
+        # Some ROMs have a superflous 512 byte header that we must skip.
+        if file_size % 1024 :
+            f.seek (512)
+            file_size -= 512
 
-            # Some ROMs have a superflous 512 byte header that we must skip.
-            if file_size % 1024 :
-                f.seek (512)
-                file_size -= 512
-
-            rom = numpy.fromfile (f, dtype=numpy.uint8)
-
-    except :
-        print str (err)
+        rom = numpy.fromfile (f, dtype=numpy.uint8)
+        _read_header ()
 
 
 ##
@@ -37,15 +47,11 @@ if __name__ == '__main__' :
     try :
         opts, args = getopt.getopt (sys.argv[1:], '')
     except :
-        print str (err)
         print 'Usage: sms.py rom'
         sys.exit (2)
 
-    cpu = cpu.CPU ()
-    memory = mapper.Mapper ()
+    readRom (args[0])
+    if rom is None :
+        exit (2)
 
-    if not _load_rom (args[0], memory) :
-        sys.exit (2)
-
-    cpu.step (memory)
-    cpu.step (memory)
+    pprint.pprint (header)
