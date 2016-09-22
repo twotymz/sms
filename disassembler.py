c62c89d53303dce229c1596263e1a1cac79d6087
twotymz <joshua.p.may@gmail.com>
2016-09-22 19:38:15 -0400

    Work on the state machine for the disassembler

--- disassembler.py
+++ disassembler.py
@@ -4,34 +4,10 @@ import getopt
 import sms
 import sys
 
+STATE_FIND_MAIN = 0
+STATE_PROGRAM = 1
 
-class Label(object) :
-
-    def __init__(self, name) :
-        self.name = name
-        self.instructions = []
-
-
-labels = {
-    0x00 : Label ('_START'),
-    0x66 : Label ('_NMI_HANDLER'),
-    0x38 : Label ('_IRQ_HANDLER')
-}
-
-
-def _add_label (addr) :
-    global labels
-    if addr not in labels :
-        labels[addr] = Label ('_LABEL_{0:X}_{1}'.format (addr, len (labels)))
-
-
-def _dump_label (addr, label) :
-
-    pc = addr
-    while pc < len (sms.rom) :
-        c, prefix, opcode, displacement, immediate, mnemonic = decode.decode (pc)
-        pc += c
-
+state = STATE_FIND_MAIN
 
 if __name__ == '__main__' :
 
@@ -42,23 +18,21 @@ if __name__ == '__main__' :
 
     sms.loadRom (sys.argv[1])
 
-    current_label = labels[0]
+    # Get a list of labels by looking for calls and jmps.
+
     pc = 0
     while pc < len (sms.rom) :
 
         c, prefix, opcode, displacement, immediate, mnemonic = decode.decode (pc)
-        print '{0:04X} \t{1:06X} {2}'.format (pc, (prefix << 8) | opcode, mnemonic)
+        #print '{0:04X} \t{1:06X} {2}'.format (pc, (prefix << 8) | opcode, mnemonic)
         pc += c
 
         if prefix == 0 :
 
             if opcode == 0xC3 :
-                #_add_label (immediate)
-                pass
+                if state == STATE_FIND_MAIN :
+                    print 'main is at 0x{0:04X}'.format (immediate)
+                    state= STATE_PROGRAM
 
-            elif opcode in (0x18, 0x20, 0x28, 0x30, 0x38) :
-                #_add_label (pc + displacement)
-                pass
-
-            elif opcode in (0xC4, 0xCC, 0xCD, 0xD4, 0xDC, 0xE4, 0xEC, 0xF4, 0xFC) :
-                _add_label (immediate)
+                elif opcode in (0xC4, 0xCC, 0xCD, 0xD4, 0xDC, 0xE4, 0xEC, 0xF4, 0xFC) :
+                    pass
