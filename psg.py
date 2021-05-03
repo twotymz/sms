a1236470434775056a61fb94a03cf14531d5fb5a
Josh <joshua.p.may@gmail.com>
2021-05-03 00:25:16 -0400

    more psg work

--- psg.py
+++ psg.py
@@ -3,32 +3,38 @@ from dataclasses import dataclass
 
 @dataclass
 class Noise:
-    reg: int = 0
+    reg: int = 0        # 3 bits of noise info
+
+    def latched(self, data):
+        self.reg = self.reg & 0x7
 
     def write(self, data):
-        # TODO...
-        self.reg = data & 0x7
+        self.reg = self.reg & 0x7
 
 
 @dataclass
 class Tone:
-    reg: int = 0
+    reg: int = 0        # 10 bit of tone info
+
+    def latched(self, data):
+        self.reg = data & 0xF
 
     def write(self, data):
-        pass
+        self.reg = (self.reg | (data << 6)) & 0x3FF
 
 
 @dataclass
 class Volume:
-    reg: int = 0xF
+    reg: int = 0xF      # 4 bits of attenuation, 0x0 = full, 0xF = silence
 
     def write(self, data):
-        pass
+        self.reg = data & 0xF
 
 
 class PSG:
 
     def __init__(self):
+
         self._channels = [
             (Volume(), Tone()),
             (Volume(), Tone()),
@@ -36,6 +42,7 @@ class PSG:
             (Volume(), Noise())
         ]
 
+        self._latched = None
 
     def write(self, byte):
         if byte & 0x7:
@@ -46,10 +53,10 @@ class PSG:
 
             volume, tone = self._channels[channel]
             if type:
-                volume.write(data)
+                self._latched = volume
             else:
-                tone.write(data)
+                self._latched = tone
+
+            self._latched.write(data)
 
         else:
-            # data byte
-            data = byte & 0x3F
